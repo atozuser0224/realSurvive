4236fe3189812ef979f6ee4710d38e9a62d64b17
Mr byeon <124233192+AtoZuser0224@users.noreply.github.com>
2025-01-15 19:33:26 +0900

    patch note add

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,11 @@ import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "2.0.0-beta.13"
   id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
   id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0" // Generates plugin.yml based on the Gradle config
 
   // Shades and relocates dependencies into our plugin jar. See https://imperceptiblethoughts.com/shadow/introduction/
-  id("com.gradleup.shadow") version "8.3.5"
+  id("com.gradleup.shadow") version "8.3.3"
   kotlin("jvm")
 }
 
@@ -30,20 +29,23 @@ tasks.assemble {
  */
 
 dependencies {
-  paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
   // paperweight.foliaDevBundle("1.21.4-R0.1-SNAPSHOT")
   // paperweight.devBundle("com.example.paperfork", "1.21.4-R0.1-SNAPSHOT")
 
   // Shadow will include the runtimeClasspath by default, which implementation adds to.
   // Dependencies you don't want to include go in the compileOnly configuration.
   // Make sure to relocate shaded dependencies!
-  implementation("dev.jorel:commandapi-bukkit-kotlin:9.5.2")
-  implementation("dev.jorel:commandapi-bukkit-shade:9.5.2")
+  implementation("dev.jorel:commandapi-bukkit-kotlin:9.7.0")
+  implementation("dev.jorel:commandapi-bukkit-shade:9.7.0")
   implementation(kotlin("stdlib-jdk8"))
+  compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 
 }
 
 tasks {
+  runServer{
+    minecraftVersion("1.21.4")
+  }
   compileJava {
     // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
     // See https://openjdk.java.net/jeps/247 for more information.
@@ -69,9 +71,7 @@ tasks {
     // relocate cloud and it's transitive dependencies
     reloc("org.incendo.cloud")
     reloc("io.leangen.geantyref")
-    dependencies {
-      include(dependency("dev.jorel:commandapi-bukkit-shade:9.5.2"))
-    }
+
 
     // TODO: Change this to my own package name
     relocate("dev.jorel.commandapi", "io.papermc.paperweight.testplugin.commandapi")
@@ -84,10 +84,14 @@ bukkitPluginYaml {
   main = "io.papermc.paperweight.testplugin.TestPlugin"
   load = BukkitPluginYaml.PluginLoadOrder.STARTUP
   authors.add("Author")
-  apiVersion = "1.21.4"
+  apiVersion = "1.21"
 }
 repositories {
   mavenCentral()
+  maven(url = "https://repo.codemc.org/repository/maven-public/")
+  maven("https://repo.papermc.io/repository/maven-public/") {
+    name = "papermc-repo"
+  }
 }
 kotlin {
   jvmToolchain(21)
--- src/main/java/io/papermc/paperweight/testplugin/TestPlugin.kt
+++ src/main/java/io/papermc/paperweight/testplugin/TestPlugin.kt
@@ -2,12 +2,19 @@ package io.papermc.paperweight.testplugin
 
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
-import io.papermc.paperweight.testplugin.sethome.setHomeCommand
+import dev.jorel.commandapi.kotlindsl.commandTree
+import dev.jorel.commandapi.kotlindsl.literalArgument
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import org.bukkit.Server
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 import org.checkerframework.checker.nullness.qual.NonNull
 import org.checkerframework.framework.qual.DefaultQualifier
+import java.util.*
 
 @DefaultQualifier(NonNull::class)
 class TestPlugin : JavaPlugin(), Listener {
@@ -17,15 +24,57 @@ class TestPlugin : JavaPlugin(), Listener {
     }
 
     override fun onLoad() {
-        instance = this
         CommandAPI.onLoad(CommandAPIBukkitConfig(this).verboseOutput(true)) // Load with verbose output
-
-        setHomeCommand(this)
+        commandTree("set"){
+            literalArgument("home"){
+                playerExecutor { player, commandArguments ->
+                    player.setHomeLocation = player.location
+                    player.sendMessage("""
+          $PREFIX set home 설정하였습니다.
+          $PREFIX ${player.location.blockX} , ${player.location.blockY} , ${player.location.blockZ}
+        """.trimIndent())
+                }
+            }
+        }
+        commandTree("home"){
+            playerExecutor { player, commandArguments ->
+                if (player.persistentDataContainer.has(setHomeAmountKey)){
+                    val amount = player.pdc.get(setHomeAmountKey, PersistentDataType.INTEGER)!!
+                    if (amount > 0){
+                        player.persistentDataContainer.set(setHomeAmountKey, PersistentDataType.INTEGER , amount - 1)
+                        player.teleportAsync(player.setHomeLocation)
+                        player.sendMessage("$PREFIX 성공적으로 set Home을 실행하였습니다\n 남은 횟수" +
+                                "${amount-1}")
+                        player.pdc.set(setHomeDate, PersistentDataType.STRING,DateManager.getCurrentDateAsString())
+                    }else if (DateManager.dateToInt(Date()) > DateManager.dateToInt(DateManager.parseStringToDate(player.pdc.get(setHomeDate, PersistentDataType.STRING)!!)!!)) {
+                        player.pdc.set(setHomeAmountKey, PersistentDataType.INTEGER,9)
+                        player.teleportAsync(player.setHomeLocation)
+                        player.sendMessage("$PREFIX 성공적으로 set Home을 실행하였습니다\n 남은 횟수" +
+                                " : ${9}")
+                    }else{
+                        player.sendMessage("$PREFIX 오늘의 제한을 모두 사용하셨습니다 내일 사용해주세요.")
+                    }
+                }else{
+                    player.pdc.set(setHomeAmountKey, PersistentDataType.INTEGER,9)
+                    player.teleportAsync(player.setHomeLocation)
+                    player.sendMessage("$PREFIX 성공적으로 set Home을 실행하였습니다\n 남은 횟수" +
+                            " : ${9}")                }
+            }
+        }
+        commandTree("patch"){
+            playerExecutor { player, commandArguments ->
+                config.getList("patch")?.forEach {
+                    player.sendMessage(it as String)
+                }
+            }
+        }
     }
 
     override fun onEnable() {
+        server.pluginManager.registerEvents(this,this)
         CommandAPI.onEnable()
-
+        instance = this
+        saveDefaultConfig()
         // Register commands, listeners etc.
     }
 
@@ -33,4 +82,8 @@ class TestPlugin : JavaPlugin(), Listener {
         CommandAPI.onDisable()
     }
 
+    @EventHandler
+    fun onJoin(e : PlayerJoinEvent){
+        e.player.sendMessage("$PREFIX 패치노트를 확인하시려면 /patch를 사용해주세요.")
+    }
 }
